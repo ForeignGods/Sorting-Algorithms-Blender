20ffe8fadc4a80770591a946d517edb64bb37772
ForeignGods <pascal.roessler@gmx.ch>
2022-05-22 18:12:04 +0200

    Create MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,148 @@
+import bpy
+import random 
+from mathutils import Vector, Matrix
+
+#variables
+count = 50 
+cubes=[]
+
+#delete every existing node_group 
+for grp in bpy.data.node_groups:
+    bpy.data.node_groups.remove(grp)
+
+#delete every existing object
+for ob in bpy.data.objects:   
+    bpy.data.objects.remove(ob)
+    
+#add counter object, set position of counter object below other cube
+bpy.ops.mesh.primitive_cube_add(location = (-2.5, 0, -3.375)) 
+bpy.context.active_object.name = 'Counter'
+
+#add geometry node modifier
+bpy.ops.object.modifier_add(type='NODES')
+
+#get and clear node_group
+node_grp = bpy.data.node_groups[-1] 
+node_grp.nodes.clear()
+
+#add nodes
+stringToCurves = node_grp.nodes.new("GeometryNodeStringToCurves")
+fillCurve = node_grp.nodes.new("GeometryNodeFillCurve")
+transform = node_grp.nodes.new("GeometryNodeTransform")
+joinStrings = node_grp.nodes.new("GeometryNodeStringJoin")
+comparisonString = node_grp.nodes.new("FunctionNodeInputString")
+comparisonCounter = node_grp.nodes.new("FunctionNodeValueToString")
+arrayString = node_grp.nodes.new("FunctionNodeInputString")
+arrayCounter = node_grp.nodes.new("FunctionNodeValueToString")
+groupOutput = node_grp.nodes.new('NodeGroupOutput')
+
+#90 degree rotation of the counter object
+transform.inputs[2].default_value[0] = 1.5708 
+#set default values of some nodes
+comparisonString.string = "Comparisons:"
+arrayString.string = "Array Accesses:"
+stringToCurves.inputs[1].default_value = 2
+joinStrings.inputs[0].default_value = " "
+
+#connect nodes to eachother
+node_grp.links.new(fillCurve.outputs[0], groupOutput.inputs[0])
+node_grp.links.new(transform.outputs[0], fillCurve.inputs[0])
+node_grp.links.new(stringToCurves.outputs[0], transform.inputs[0])
+node_grp.links.new(joinStrings.outputs[0], stringToCurves.inputs[0])
+node_grp.links.new(comparisonCounter.outputs[0], joinStrings.inputs[1])
+node_grp.links.new(comparisonString.outputs[0], joinStrings.inputs[1])
+node_grp.links.new(arrayCounter.outputs[0], joinStrings.inputs[1])
+node_grp.links.new(arrayString.outputs[0], joinStrings.inputs[1])
+
+#fill arrays with numbers between 1 & count
+ran = list(range(1,count+1))
+
+#randomize array order
+random.shuffle(ran)
+
+#sets origin of cube to bottom of mesh
+def origin_to_bottom(ob, matrix=Matrix()):
+    me = ob.data
+    mw = ob.matrix_world
+    local_verts = [matrix @ Vector(v[:]) for v in ob.bound_box]
+    o = sum(local_verts, Vector()) / 8
+    o.z = min(v.z for v in local_verts)
+    o = matrix.inverted() @ o
+    me.transform(Matrix.Translation(-o))
+    mw.translation = mw @ o
+    
+#create cubes with random location
+for i in range(count):
+    bpy.ops.mesh.primitive_cube_add(location=(ran[i], 0, 0), scale=(0.25, 0.25, 0.25))   
+    
+#shuffle array
+random.shuffle(ran)
+  
+#assign random scale to all cubes and add them to array
+i = 0
+for ob in bpy.data.objects:
+    if ob.type == 'MESH' and ob.name != "Counter":    
+        origin_to_bottom(ob)
+        ob.scale.z = ran[i]
+        cubes.append(ob)
+        i += 1
+
+#sort array based on location.x        
+cubes.sort(key = lambda obj: obj.location.x)
+
+def mergeSort(cubes):
+    if len(cubes) > 1:
+        mid = len(cubes) // 2
+        left = cubes[:mid]
+        right = cubes[mid:]
+             
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+        
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i].scale.z <= right[j].scale.z:
+        
+                left[i].location.x = k
+                
+                # The value from the left half has been used
+                cubes[k] = left[i]
+                
+                # Move the iterator forward
+                i += 1
+            else:
+                
+                right[j].location.x = k
+             
+                cubes[k] = right[j]
+                
+                j += 1
+                # Move to the next slot
+            k += 1
+        # For all the remaining values
+        while i < len(left):
+            
+            left[i].location.x = k               
+
+            cubes[k] = left[i]
+            
+            i += 1
+            k += 1
+        while j < len(right):
+            
+            right[j].location.x = k              
+
+            cubes[k]=right[j]
+            
+            j += 1
+            k += 1
+
+#start recursion
+mergeSort(cubes)
